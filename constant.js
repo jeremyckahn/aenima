62cfc8f174c3e12a7da9c53603d0dc34f5455d3d
Jeremy Kahn <jeremyckahn@gmail.com>
2016-01-19 07:48:31 -0600

    Port HidableComponent and ModalComponent from Stylie

--- constant.js
+++ constant.js
@@ -3,8 +3,10 @@ define(function () {
 
   return {
     CUSTOM_CURVE_PREFIX: 'customCurve'
+    ,HIDABLE_VIEW_TRANSITION_DURATION: 450
     ,NEW_KEYFRAME_MS_INCREASE: 1000
     ,NEW_KEYFRAME_X_INCREASE: 200
     ,UNDO_STACK_LIMIT: 50
+    ,MODAL_OPACITY: 0.95
   };
 });
